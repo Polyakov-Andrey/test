ea0c3711147554c32b55d6b8b6016c105d034291
Andrey Polyakov <Polyakov.Andrey@gmail.com>
2012-05-14 17:53:43 +0300

    Bug Fix: Log is not written to a log file.

--- src/com/slobodastudio/loger/LoggerHelper.java
+++ src/com/slobodastudio/loger/LoggerHelper.java
@@ -9,6 +9,7 @@ import java.util.Hashtable;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  *
@@ -18,15 +19,16 @@ import java.util.logging.Logger;
 public class LoggerHelper {
 
     Hashtable<String, Logger> tableLoger = new Hashtable<String, Logger>();
-    
+
     public static Logger createFileLoger(String nameLogger, String nameFile) {
         assert (nameLogger != null) : "Name of logger is empty";
         assert (nameFile != null) : "Name of file is empty";
-        
+
         Logger log = null;
         try {
             FileHandler hand = new FileHandler(nameFile);
-            log = Logger.getLogger(nameFile);
+            hand.setFormatter(new SimpleFormatter());
+            log = Logger.getLogger(nameLogger);
             log.addHandler(hand);
         } catch (IOException ex) {
             Logger.getLogger(LoggerHelper.class.getName()).
